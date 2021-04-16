10fed32f8cfed7d08954e5d74c23f861708fe31a
Matthias Thomann <matthias.thomann@hotmail.com>
2021-04-16 12:55:39 +0200

    Added dummy compute shader with simple voronoi nosie to experiment with compute shaders.

---
+++ impl/Weather Rendering System/Assets/Scripts/CloudNoise.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51bdac47cad8bed84317d4a10063eece8a462a0becd1ad4ad396e71a24ad547b
+size 1408
---
+++ impl/Weather Rendering System/Assets/Shaders/Cloud Shader.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:347d95b4a9c554751f0541b911e1ff9bb3bb0d6087543e27d9dc238b261879ca
+size 1648

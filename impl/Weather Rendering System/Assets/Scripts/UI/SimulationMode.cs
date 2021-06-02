3b6c415554faf7f09c6df4356bc311a031b50573
Matthias Thomann <matthias.thomann@hotmail.com>
2021-06-02 21:51:39 +0200

    UI wip.

---
+++ impl/Weather Rendering System/Assets/Scripts/UI/SimulationMode.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2191c7d58482e315833ef898b5fb42a3fb1ff6f34e452792b188bf89b914102a
+size 133
---
+++ impl/Weather Rendering System/Assets/Scripts/UI/UIMaster.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10718d9c4706093bde02b1475fd7eda8ed393a5bdbb094ddffe96efdb3080852
+size 2400

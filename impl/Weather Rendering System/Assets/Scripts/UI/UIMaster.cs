029f2e40a270a1b4cd413e472822698365953b49
Matthias Thomann <matthias.thomann@hotmail.com>
2021-06-03 13:43:41 +0200

    UI.

--- impl/Weather Rendering System/Assets/Scripts/UI/UIMaster.cs
+++ impl/Weather Rendering System/Assets/Scripts/UI/UIMaster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:529b60e9a52bf0f0ae42e933c77235f160cb80ac6799e358cce74e0795f5ba14
-size 12862
+oid sha256:fb28ad1f01c7d53ccb37df7697b2132e4340851df222256215bcb2effaaca3c9
+size 12899

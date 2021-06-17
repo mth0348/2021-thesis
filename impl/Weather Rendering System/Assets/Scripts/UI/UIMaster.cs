f04caddde1dc4f1377cf317ea94404d2aa37e0e3
Matthias Thomann <matthias.thomann@hotmail.com>
2021-06-17 19:54:41 +0200

    added preset buttons for Demo.

--- impl/Weather Rendering System/Assets/Scripts/UI/UIMaster.cs
+++ impl/Weather Rendering System/Assets/Scripts/UI/UIMaster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b27c878595a473988babec5a0cb544d860bbe2faf97c88967017ea25ac7726b
-size 14263
+oid sha256:8081000cb56fd6e2bea150b22b5e1f9391cad9285bd412a67ad541c6a4a124f4
+size 14894
